014417762392207aa6c65f0b8b867638830add6b
Victor <ivanvikvik@gmail.com>
2022-03-21 10:33:05 +0200

    Added task solution

--- StepikCppProject/Main.cpp
+++ StepikCppProject/Main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+/*	Даны два целых числа. 
+	Выведите значение наибольшего из них. 
+	Если числа равны, выведите любое из них. */
+
 int main(void) {
+	int a, b;
+
+	cin >> a >> b;
+	cout << (a > b ? a : b);
 
 	return 0;
 }
