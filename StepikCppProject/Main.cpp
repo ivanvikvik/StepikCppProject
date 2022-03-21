d0b4516c3dfeea2f03740f434b11b9060dcfda21
Victor <ivanvikvik@gmail.com>
2022-03-21 10:58:06 +0200

    Added task solution

--- StepikCppProject/Main.cpp
+++ StepikCppProject/Main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
+/*
+* ƒаны три целых числа. 
+* ќпределите, сколько среди них совпадающих.
+* ѕрограмма должна вывести одно из чисел:
+* 3 (если все совпадают), 2 (если два совпадает) 
+* или 0 (если все числа различны). 
+*/
+
 int main(void) {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	int result = 0;
+
+	if (a == b && b == c) {
+		result = 3;
+	}
+	else if (a == b || b == c || a == c) {
+		result = 2;
+	}
+
+	cout << result;
 
 	return 0;
 }
