46e67784016488d3fba4b84f2458415e2f5ffbec
Victor <ivanvikvik@gmail.com>
2022-03-21 10:51:16 +0200

    Added task solution

--- StepikCppProject/Main.cpp
+++ StepikCppProject/Main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+//ConditionalStatements/task05
+
+/*
+* Даны три натуральных числа A, B, C. 
+* Определите, существует ли треугольник 
+* с такими сторонами. Если треугольник существует,
+* выведите строку YES, иначе выведите строку NO.
+*/
+
 int main(void) {
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	bool result = a + b > c && a + c > b && b + c > a;
+	cout << (result ? "YES" : "NO");
 
 	return 0;
 }
